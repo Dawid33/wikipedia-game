0ae36928aff772d4b7e7236700b5a1f44a710905
Dawid Sobczak <dawidsobczak@fastmail.com>
2022-02-14 21:25:38 +0000

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+    <main>
+      <Header/>
+      <h1>Default Content</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ frontend/src/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (  
+    <header>
+        <a href="/">Home</a>
+        <a>Users</a>
+        <a>Settings</a>
+    </header>
+    );
+  }
+  
+  export default Header;
